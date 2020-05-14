c130013f680f9900d538b788007f54ca5592714f
Denis Cherniy <d.chernyi@storytelling-software.com>
2020-05-14 14:57:01 +0300

    Added tests target.

---
+++ TripperTests/Scenes/Manage Route Map/Test Doubles/ManageRouteMapBusinessLogicSpy.swift
@@ -0,0 +1,69 @@
+//
+//  ManageRouteMapBusinessLogicSpy.swift
+//  TripperTests
+//
+//  Created by Denis Cherniy on 14.05.2020.
+//  Copyright © 2020 Denis Cherniy. All rights reserved.
+//
+
+@testable import Tripper
+
+class ManageRouteMapBusinessLogicSpy: ManageRouteMapBusinessLogic {
+    func setupData(request: ManageRouteMap.SetupData.Request) {
+    }
+    
+    func fetchDifference(request: ManageRouteMap.FetchDifference.Request) {
+    }
+    
+    func createRoutePoint(request: ManageRouteMap.CreateRoutePoint.Request) {
+    }
+    
+    func setRoutePoint(request: ManageRouteMap.SetRoutePoint.Request) {
+    }
+    
+    func selectAnnotation(request: ManageRouteMap.SelectAnnotation.Request) {
+    }
+    
+    func deselectAnnotation(request: ManageRouteMap.DeselectAnnotation.Request) {
+    }
+    
+    func showDetail(request: ManageRouteMap.ShowDetail.Request) {
+    }
+    
+    func editRoutePoint(request: ManageRouteMap.EditRoutePoint.Request) {
+    }
+    
+    func deleteRoutePoint(request: ManageRouteMap.DeleteAnnotation.Request) {
+    }
+    
+    func createRouteFragment(request: ManageRouteMap.CreateRouteFragment.Request) {
+    }
+    
+    func addRouteFragment(request: ManageRouteMap.AddRouteFragment.Request) {
+    }
+    
+    func deleteRouteFragment(request: ManageRouteMap.DeleteRouteFragment.Request) {
+    }
+    
+    func mapRoute(request: ManageRouteMap.MapRoute.Request) {
+    }
+    
+    func clearAll(request: ManageRouteMap.ClearAll.Request) {
+    }
+    
+    func toggleUserInput(request: ManageRouteMap.ToggleUserInput.Request) {
+    }
+    
+    func focus(request: ManageRouteMap.Focus.Request) {
+    }
+    
+    func focusOnRoute(request: ManageRouteMap.FocusOnRoute.Request) {
+    }
+    
+    func focusOnUser(request: ManageRouteMap.FocusOnUser.Request) {
+    }
+    
+    func routeEstimation(request: ManageRouteMap.RouteEstimation.Request) {
+    }
+    
+}
---
+++ TripperTests/Scenes/Manage Route Map/Test Doubles/ManageRouteMapDisplayLogicSpy.swift
@@ -0,0 +1,70 @@
+//
+//  ManageRouteMapDisplayLogicSpy.swift
+//  TripperTests
+//
+//  Created by Denis Cherniy on 14.05.2020.
+//  Copyright © 2020 Denis Cherniy. All rights reserved.
+//
+
+@testable import Tripper
+
+class ManageRouteMapDisplayLogicSpy: ManageRouteMapDisplayLogic {
+    func displayDataSetup(viewModel: ManageRouteMap.SetupData.ViewModel) {
+    }
+    
+    func displayFetchDifference(viewModel: ManageRouteMap.FetchDifference.ViewModel) {
+    }
+    
+    func displayCreateRoutePoint(viewModel: ManageRouteMap.CreateRoutePoint.ViewModel) {
+    }
+    
+    func displaySetRoutePoint(viewModel: ManageRouteMap.SetRoutePoint.ViewModel) {
+    }
+    
+    func displaySelectAnnotation(viewModel: ManageRouteMap.SelectAnnotation.ViewModel) {
+    }
+    
+    func displayDeselectAnnotation(viewModel: ManageRouteMap.DeselectAnnotation.ViewModel) {
+    }
+    
+    func displayShowDetail(viewModel: ManageRouteMap.ShowDetail.ViewModel) {
+    }
+    
+    func displayEditRoutePoint(viewModel: ManageRouteMap.EditRoutePoint.ViewModel) {
+    }
+    
+    func displayDeleteRoutePoint(viewModel: ManageRouteMap.DeleteAnnotation.ViewModel) {
+    }
+    
+    func displayCreateRouteFragment(viewModel: ManageRouteMap.CreateRouteFragment.ViewModel) {
+    }
+    
+    func displayAddRouteFragment(viewModel: ManageRouteMap.AddRouteFragment.ViewModel) {
+    }
+    
+    func displayDeleteRouteFragment(viewModel: ManageRouteMap.DeleteRouteFragment.ViewModel) {
+    }
+    
+    func displayMapRoute(viewModel: ManageRouteMap.MapRoute.ViewModel) {
+    }
+    
+    func displayClearAll(viewModel: ManageRouteMap.ClearAll.ViewModel) {
+    }
+    
+    func displayToggleUserInput(viewModel: ManageRouteMap.ToggleUserInput.ViewModel) {
+    }
+    
+    func displayFocus(viewModel: ManageRouteMap.Focus.ViewModel) {
+    }
+    
+    func displayFocusOnRoute(viewModel: ManageRouteMap.FocusOnRoute.ViewModel) {
+    }
+    
+    func displayFocusOnUser(viewModel: ManageRouteMap.FocusOnUser.ViewModel) {
+    }
+    
+    func displayRouteEstimation(viewModel: ManageRouteMap.RouteEstimation.ViewModel) {
+    }
+    
+    
+}
