e59a1fa1cfba6a2741c77ff5083ff8847ea382c3
Denis Cherniy <d.chernyi@storytelling-software.com>
2020-05-15 16:06:32 +0300

    Added mocks for gateways.

---
+++ TripperTests/Scenes/Service Doubles/RouteFragmentGatewayMock.swift
@@ -0,0 +1,9 @@
+//
+//  RouteFragmentGatewayMock.swift
+//  TripperTests
+//
+//  Created by Denis Cherniy on 14.05.2020.
+//  Copyright © 2020 Denis Cherniy. All rights reserved.
+//
+
+import Foundation
---
+++ TripperTests/Scenes/Service Doubles/RoutePointGatewayMock.swift
@@ -0,0 +1,9 @@
+//
+//  RoutePointGatewayMock.swift
+//  TripperTests
+//
+//  Created by Denis Cherniy on 14.05.2020.
+//  Copyright © 2020 Denis Cherniy. All rights reserved.
+//
+
+import Foundation
